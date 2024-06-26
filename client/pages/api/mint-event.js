ea963ad0bd7aed3c9125f80b2f08dc049195d0f5
TForge1 <TraitForge@protonmail.com>
2024-06-26 10:47:10 +0930

    log testing

--- client/pages/api/mint-event.js
+++ client/pages/api/mint-event.js
@@ -1,22 +1,28 @@
-import { contractsConfig } from '@/utils/contractsConfig';
-import { JsonRpcProvider, ethers } from 'ethers';
 
 export default async function handler(req, res) {
   try {
     const { event } = req.body;
     const activity = event.activity[0];
+
     if (activity.fromAddress === '0x0000000000000000000000000000000000000000') {
       // Mints new NFT
       const tokenId = Number(activity.erc721TokenId);
-      const infuraProvider = new JsonRpcProvider(contractsConfig.infuraRPCURL);
-      const nftContract = new ethers.Contract(
-        contractsConfig.traitForgeNftAddress,
-        contractsConfig.traitForgeNftAbi,
-        infuraProvider
-      );
+      const price = Number(activity.price);
+      const fromAddress = activity.fromAddress;
+      const toAddress = activity.toAddress;
+      const value = Number(activity.value);
 
+      // Log the details
+      console.log(`Token ID: ${tokenId}`);
+      console.log(`From Address: ${fromAddress}`);
+      console.log(`To Address: ${toAddress}`);
+      console.log(`Value: ${value}`);
+      console.log(`Price: ${price}`);
+
+      res.status(200).send('Ok');
+    } else {
+      res.status(400).send('Invalid fromAddress');
     }
-    res.status(200).send('Ok');
   } catch (e) {
     console.log('Nft event error:', e);
     res.status(500).send(e);
